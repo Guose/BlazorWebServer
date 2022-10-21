4dd174a8397fabe2c5a365a061de4e39552c9022
Justin Elder <guose79@gmail.com>
2022-10-21 01:19:43 -0700

    Add files via upload

---
+++ DataAccess/AddRecordsToAvailableAppointment.sql
@@ -0,0 +1,2 @@
+insert into blazor_schedule.available_appointment(Reserved, StartDate, StartTime, EndTime, CreatedAt, UpdatedAt) 
+values (true, '2022-09-29', '2022-09-29 15:00:00', '2022-09-29 15:30:00', NOW(), NOW());
